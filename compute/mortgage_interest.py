095f9737563eb756ddaa3c72787f54352fdea7b2
FrenchCommando <martialren@gmail.com>
2024-06-08 12:50:57 -0400

    init commit

---
+++ compute/mortgage_interest.py
@@ -0,0 +1,11 @@
+import logging
+# compute mortgage interest
+# compute tax deduction impact to determine effective mortgage interest
+
+
+def main():
+    logging.log(level=logging.INFO, msg="I'm computing mortgage interest")
+
+
+if __name__ == '__main__':
+    main()
---
+++ main.py
@@ -0,0 +1,11 @@
+import json
+import logging
+from compute.mortgage_interest import main
+
+
+def print_hi(name):
+    print(f'Hi, {name}')
+
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
